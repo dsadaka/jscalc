e6d4dfbd5ab73b484aa603189be9ba9f862eb981
Dan Sadaka <dan@web-site1.com>
2018-03-27 19:08:41 -0400

    have UI done, need to implement exponentiation

--- app/javascript/packs/calc.jsx
+++ app/javascript/packs/calc.jsx
@@ -25,7 +25,7 @@ class Calculator extends React.Component {
              if (this.state.input.length > 0) {
                  this.pushHistory(this.state.input + ' = ' + this.state.output);
                  this.setState({
-                     input: this.state.output,
+                     input: this.state.output.toString(),
                      output: '',
                      operator: null,
                      deleteToggle: 'CLR'
@@ -33,7 +33,7 @@ class Calculator extends React.Component {
              }
         } else if (this.state.operator !== null) {
             var newInput = this.state.input + value;
-            var replace = newInput.replace(/x/g, '*').replace(/÷/g, '/');
+            var replace = newInput.replace(/x/g, '*').replace(/÷/g, '/').replace(/√x/, '**');
             var result = Math.round(10000000 * eval(replace),5) / 10000000;
             this.setState({
                 input: newInput,
@@ -48,9 +48,6 @@ class Calculator extends React.Component {
         }
     }
     enterOperator(value) {
-        this.setState({
-            input: this.state.input.toString()
-        });
         if(value == 'CLR') {
             this.setState({
                 input: '',
@@ -61,6 +58,27 @@ class Calculator extends React.Component {
             this.setState({
                 input: this.state.input.slice(0, -1)
             })
+        } else if(value == 'CA') {
+            this.clearHistory();
+            this.setState({
+                input: '',
+                output: '',
+                operator: null,
+            })
+        } else if(value == 'SQRT') {
+            if (this.state.input.length > 0) {
+                var replace = this.state.input.replace(/x/g, '*').replace(/÷/g, '/');
+                var result = this.round(Math.pow(eval(replace), 1 / 2)).toString();
+                this.pushHistory('√(' + replace + ') = ' + result);
+                this.setState({
+                    input: result,
+                    output: '',
+                    operator: null,
+                    deleteToggle: 'CLR'
+                })
+
+            }
+
         } else {
             this.setState({
                 input: this.state.input + value,
@@ -75,12 +93,17 @@ class Calculator extends React.Component {
             this.state.history = history
         }                
     }
+    clearHistory() {
+        this.removeCookie(this.cookieName);
+        this.setState({
+            history: []
+        })
+    }
+
     pushHistory(value) {
         this.setHistory();
-        while (this.state.history.length >= 10) {
-            this.state.history.shift(1)
-        }
-        this.state.history.push(value)
+        while (this.state.history.length >= 10) this.state.history.shift(1);
+        this.state.history.push(value);
         this.setCookie(this.cookieName, this.state.history)
     }
     getCookie(name) {
@@ -98,6 +121,9 @@ class Calculator extends React.Component {
     removeCookie(name) {
         const cookies = new Cookies();
         cookies.remove(name)
+    }
+    round(expr) {
+        return Math.round(10000000 * expr,5) / 10000000
     }
     render() {
         return(
@@ -126,12 +152,35 @@ class CalcInput extends React.Component {
     render(props) {
         return(
             <div>
+                <FnKeys className="keypad fnkey" enterOperator={this.props.enterOperator} deleteToggle={this.props.deleteToggle} />
                 <KeyPad className="keypad" enterDigit={this.props.enterDigit} />
                 <Operators  enterOperator={this.props.enterOperator} deleteToggle={this.props.deleteToggle} />
                 <ExtendedOperators enterDigit={this.props.enterDigit} />
+
             </div>
         )
     }
+}
+
+class FnKeys extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <div className="fnkey">
+                <div className="keyrow">
+                    <Key value={this.props.deleteToggle} enterValue={this.props.enterOperator} />
+                    <Key value='CA' enterValue={this.props.enterOperator} />
+                    <Key value=' ' disp="&nbsp;" enterValue={Function.prototype} />
+                    <Key value='SQRT' disp="<b>√</b>x" enterValue={this.props.enterOperator} />
+                    <div className="fnkey-filler">&nbsp;</div>
+                </div>
+            </div>
+        )
+    }
+
 }
 
 class KeyPad extends React.Component {
@@ -186,12 +235,29 @@ class HistRow extends React.Component {
 class Key extends React.Component {
     render() {
         const value = this.props.value;
+        const disp = typeof this.props.disp == 'undefined' ? this.props.value : this.props.disp
         return(
-            <button className="key waves-effect waves-circle waves-light" onClick={this.props.enterValue.bind(this, value)}>{value}</button>
+            <button className="key waves-effect waves-circle waves-light" value={value} onClick={this.props.enterValue.bind(this, value)}>
+                <RawHTML str={disp} /></button>
         )
     }
 }
 
+class RawHTML extends React.Component {
+    constructor() {
+        super();
+    }
+
+    rawMarkup(str) {
+        return { __html: str };
+    }
+    render() {
+        const str = this.props.str;
+        return(
+            <span dangerouslySetInnerHTML={this.rawMarkup(str)} />
+        )
+    }
+}
 class Operators extends React.Component {
     constructor() {
         super();
@@ -200,7 +266,7 @@ class Operators extends React.Component {
     render() {
         return(
             <div className="operators">
-                <Key value={this.props.deleteToggle} enterValue={this.props.enterOperator} />
+                <Key value='**' disp='x<i>y</i>' enterValue={this.props.enterOperator} />
                 <Key value='÷' enterValue={this.props.enterOperator} />
                 <Key value='x' enterValue={this.props.enterOperator} />
                 <Key value='-' enterValue={this.props.enterOperator} />
