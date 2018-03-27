978e2155c32433cd126a750bc46c273b6f505fb0
Dan Sadaka <dan@web-site1.com>
2018-03-26 23:46:09 -0400

    have history working, need to add exponentiation and sqrt

--- app/javascript/packs/calc.jsx
+++ app/javascript/packs/calc.jsx
@@ -1,27 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Cookies from 'universal-cookie'
+
 class Calculator extends React.Component {
     constructor() {
         super();
+        this.cookieName = this.cookieName
         this.enterDigit = this.enterDigit.bind(this);
         this.enterOperator = this.enterOperator.bind(this);
+        this.pushHistory = this.pushHistory.bind(this);
+        this.setHistory = this.setHistory.bind(this);
         this.state = {
             input: '',
             output: '',
+            history: [],
             operator: null,
             deleteToggle: 'DEL'
         }
+        this.setHistory();
+        
     }
     enterDigit(value) {
          if(value === '=') {
-            this.setState({
-                input: this.state.output,
-                output: '',
-                operator: null,
-                deleteToggle: 'CLR'
-            })
+             if (this.state.input.length > 0) {
+                 this.pushHistory(this.state.input + ' = ' + this.state.output);
+                 this.setState({
+                     input: this.state.output,
+                     output: '',
+                     operator: null,
+                     deleteToggle: 'CLR'
+                 })
+             }
         } else if (this.state.operator !== null) {
             var newInput = this.state.input + value;
             var replace = newInput.replace(/x/g, '*').replace(/÷/g, '/');
-            var result = eval(replace);
+            var result = Math.round(10000000 * eval(replace),5) / 10000000;
             this.setState({
                 input: newInput,
                 output: result,
@@ -35,6 +48,9 @@ class Calculator extends React.Component {
         }
     }
     enterOperator(value) {
+        this.setState({
+            input: this.state.input.toString()
+        });
         if(value == 'CLR') {
             this.setState({
                 input: '',
@@ -53,10 +69,40 @@ class Calculator extends React.Component {
 
         }
     }
+    setHistory() {
+        var history  = this.getCookie(this.cookieName)
+        if (typeof history != 'undefined') {
+            this.state.history = history
+        }                
+    }
+    pushHistory(value) {
+        this.setHistory();
+        while (this.state.history.length >= 10) {
+            this.state.history.shift(1)
+        }
+        this.state.history.push(value)
+        this.setCookie(this.cookieName, this.state.history)
+    }
+    getCookie(name) {
+        const cookies = new Cookies();
+        var cookie = cookies.get(name);
+        console.log(cookie)
+
+        return cookie
+    }
+    setCookie(name, value) {
+        const cookies = new Cookies();
+        this.removeCookie(name);
+        cookies.set(name, value)
+    }
+    removeCookie(name) {
+        const cookies = new Cookies();
+        cookies.remove(name)
+    }
     render() {
         return(
             <div>
-                <Display input={this.state.input} output={this.state.output} />
+                <Display input={this.state.input} output={this.state.output} history={this.state.history}/>
                 <CalcInput  enterDigit={this.enterDigit} enterOperator={this.enterOperator} deleteToggle={this.state.deleteToggle} />
             </div>
         )
@@ -69,6 +115,7 @@ class Display extends React.Component {
     render() {
         return(
             <div className="display z-depth-3">
+                <div className="history"><History history={this.props.history} /></div>
                 <div className="input">{this.props.input.toString()}</div>
                 <div className="output">{this.props.output}</div>
             </div>
@@ -108,6 +155,31 @@ class KeyPad extends React.Component {
 
             </div>
         )
+    }
+}
+
+class History extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            this.props.history.map(h => <HistRow key={h} value={h} />)
+        )
+    }
+
+}
+
+class HistRow extends React.Component {
+    render() {
+        const value = this.props.value;
+        return(
+            <div>
+                {value}
+                <br></br>
+            </div>
+        )
+        
     }
 }
 
